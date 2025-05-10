74e2287ceb5bf81ccac41630e522f6f6aa6131a7
owais825 <owais825@gmail.com>
2025-05-10 12:59:04 +0530

    feat: adding final data

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RAG.ai
 This is a retrieval and generation tool that looks into pdfs/docs and generate insights
+About hF embeddings
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
 #api config
-genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
 gemini_model=genai.GenerativeModel('gemini-2.0-flash')
 
 # Cache the HF embeddings to avoid slow reload of embeddings
@@ -21,7 +21,45 @@ def embeddings():
 embedding_model = embeddings()
 
 # userinterface
-st.header("RAG Assistant: orange[HF embedings + Gemini LLM]")
+st.header("RAG Assistant: HF embedings + Gemini LLM")
 st.subheader("Your personal assistant for document analysis and Q&A")
 uploaded_file = st.file_uploader(label="Upload a PDF file", type="pdf")
-if 
+if uploaded_file:
+    raw_text = ""
+    pdf = PdfReader(uploaded_file)
+    for i,page in enumerate(pdf.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+    if raw_text.strip():
+        document = Document(page_content=raw_text)
+        # using CharacterTextSplitter to split the document into smaller chunks and pass it to model
+        splitter= CharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        chunks = splitter.split_documents([document])
+        
+        # store the chunks into FAISS vectorstore
+        chunk_pieces = [chunk.page_content for chunk in chunks]
+        vectordb = FAISS.from_texts(chunk_pieces, embedding_model) # convert them into vector embeddings
+        retriever = vectordb.as_retriever() # retrieve the vector embeddings
+        
+        st.success("Embeddings are generated.Ask your questions!")
+        user_input = st.text_input(label="Ask a question :")
+        if user_input:
+            with st.chat_message('user'):
+                st.write(user_input)
+                
+            with st.spinner("Generating response..."):
+                relevant_docs= retriever.get_relevant_documents(user_input)
+                context = '\n\n'.join([doc.page_content for doc in relevant_docs])
+                prompt = f'''You are an expert assistant. Use the context below to answer the query.
+                if unsure or information not avialable in the doc.Look into other sources
+                context:{context}
+                query:{user_input} Answer:'''
+                response = gemini_model.generate_content(prompt)
+                st.markdown('Answer: ')
+                st.write(response.text)
+    else:
+        st.warning("No text could be extracted from PDF.Please upload a readable PDF file.")
+                
+            
+
